9a0bb783d92e15bab05c27e38710694e19d6919f
Julian Jurec <julian.jurec@gmail.com>
2026-05-21 18:40:38 +0200

    add fzf-tab

--- run_onchange_setup.sh
+++ run_onchange_setup.sh
@@ -309,6 +309,7 @@ fi
 
 # Install oh-my-zsh custom plugins
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+[ -d "$ZSH_CUSTOM/plugins/fzf-tab" ] || git clone https://github.com/Aloxaf/fzf-tab "$ZSH_CUSTOM/plugins/fzf-tab"
 [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] || git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 [ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 [ -d "$ZSH_CUSTOM/plugins/command-time" ] || git clone https://github.com/popstas/zsh-command-time "$ZSH_CUSTOM/plugins/command-time"
